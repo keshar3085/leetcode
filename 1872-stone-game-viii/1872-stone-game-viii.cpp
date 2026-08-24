ef7460e6f74617ec8229ff03fc773c859bfe95fa
Keshar Sahu <kesharsahu3085@gmail.com>
2026-08-24 19:15:37 +0530

    Time: 90 ms (95.17%), Space: 87.2 MB (84.19%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        int sum =0;
+        for(int x:stones)
+            sum += x;
+        int dp = sum;
+        for (int i = n-2;i>=1;i--){
+            sum -= stones[i+1];
+            dp = max(sum-dp,dp);
+        }
+        return dp;
+    }
+};
